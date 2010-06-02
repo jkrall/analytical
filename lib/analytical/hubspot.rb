2cf528d73d97374bfdec08c4988658c16f3251cd
Joshua Krall <joshuakrall@pobox.com>
2010-06-02 11:14:04 -0500

    Fixing broken hubspot

--- lib/analytical/hubspot.rb
+++ lib/analytical/hubspot.rb
@@ -15,7 +15,7 @@ module Analytical
         <script type="text/javascript" language="javascript">
         var hs_portalid=#{options[:portal_id]};
         var hs_salog_version = "2.00";
-        var hs_ppa = "options[:domain]";
+        var hs_ppa = "#{options[:domain]}";
         document.write(unescape("%3Cscript src='" + document.location.protocol + "//" + hs_ppa + "/salog.js.aspx' type='text/javascript'%3E%3C/script%3E"));
         </script>
         HTML
