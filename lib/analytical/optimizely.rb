825abbd34d08c078d1bc8792afc10f9e80bd1e83
Nathan Phelps <me@nathanwphelps.com>
2010-08-21 13:28:32 +0800

    Added Optimizely module

---
+++ lib/analytical/optimizely.rb
@@ -0,0 +1,23 @@
+module Analytical
+  module Optimizely
+    class Api
+      include Analytical::Base::Api
+
+      def initialize(parent, options={})
+        super
+        @tracking_command_location = :head_prepend
+      end
+
+      def init_javascript(location)
+        init_location(location) do
+          js = <<-HTML
+          <!-- Analytical Init: Optimizely -->
+          <script type="text/javascript">document.write(unescape("%3Cscript src='"+document.location.protocol+"//cdn.optimizely.com/js/#{options[:key]}.js' type='text/javascript'%3E%3C/script%3E"));</script>
+          HTML
+          js
+        end
+      end
+
+    end
+  end
+end
---
+++ spec/analytical/optimizely_spec.rb
@@ -0,0 +1,40 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe "Analytical::Optimizely::Api" do
+  before(:each) do
+    @parent = mock('api', :options=>{:optimizely=>{:key=>'abc'}})
+  end
+  describe 'on initialize' do
+    it 'should set the command_location' do
+      a = Analytical::Optimizely::Api.new @parent, {:key=>'abc'}
+      a.tracking_command_location.should == :head_prepend
+    end
+    it 'should set the options' do
+      a = Analytical::Optimizely::Api.new @parent, {:key=>'abc'}
+      a.options.should == {:key=>'abc'}
+    end
+  end
+  describe '#track' do
+    it 'should return the tracking javascript' do
+      @api = Analytical::Optimizely::Api.new @parent, {:key=>'abcdef'}
+      @api.track('pagename', {:some=>'data'}).should == ''
+    end
+  end
+
+  describe '#identify' do
+    it 'should return an empty string' do
+      @api = Analytical::Optimizely::Api.new @parent, {:key=>'abcdef'}
+      @api.identify('nothing', {:matters=>'at all'}).should == ''
+    end
+  end
+
+  describe '#init_javascript' do
+    it 'should return the init javascript' do
+      @api = Analytical::Optimizely::Api.new @parent, {:key=>'abcdef'}
+      @api.init_javascript(:head_prepend).should =~ /cdn\.optimizely.com\/js\/abcdef\.js/
+      @api.init_javascript(:head_append).should == ''
+      @api.init_javascript(:body_prepend).should == ''
+      @api.init_javascript(:body_append).should == ''
+    end
+  end
+end
