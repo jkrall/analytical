51ef65ea98e5a9a33f8a3dc675ad930e1379300a
Joshua Krall <joshuakrall@pobox.com>
2010-09-11 18:08:35 -0500

    Updating example rails app... demonstrates session-store stuff, upgraded to rails3, and fixed incompatibilities.

--- example/app/controllers/application_controller.rb
+++ example/app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
 
-  analytical :modules=>[:console, :google, :clicky], :disable_if=>lambda{false}
+  analytical :modules=>[:console, :google, :clicky], :disable_if=>lambda{false}, :use_session_store=>true
 end
