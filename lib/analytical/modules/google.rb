41b2c79a684b3a3a3167989d6b04966ea28aca13
Ablyamitov Ablyamit <ablyamyt@gmail.com>
2011-02-28 18:51:04 +0200

    add domain option

--- lib/analytical/modules/google.rb
+++ lib/analytical/modules/google.rb
@@ -14,7 +14,7 @@ module Analytical
           <!-- Analytical Init: Google -->
           <script type="text/javascript">
             var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', '#{options[:key]}'], ['_trackPageview']);
+            _gaq.push(['_setAccount', '#{options[:key]}'], ['_setDomainName', '#{options[:domain]}'], ['_trackPageview']);
             (function() {
               var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
               ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
