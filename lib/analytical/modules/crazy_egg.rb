3ee95d3a4dcaf3be4404f7bdd5c33d7967d939b9
Joshua Krall <joshuakrall@pobox.com>
2011-03-15 01:17:49 -0500

    Fixing crazyegg if key is numeric Closes #11

--- lib/analytical/modules/crazy_egg.rb
+++ lib/analytical/modules/crazy_egg.rb
@@ -10,7 +10,7 @@ module Analytical
 
       def init_javascript(location)
         init_location(location) do
-          code_url = "#{options[:key][0,4]}/#{options[:key][4,4]}"
+          code_url = "#{options[:key].to_s[0,4]}/#{options[:key].to_s[4,4]}"
           js = <<-HTML
           <!-- Analytical Init: CrazyEgg -->
           <script type="text/javascript" src="#{protocol}://s3.amazonaws.com/new.cetrk.com/pages/scripts/#{code_url}.js"> </script>
