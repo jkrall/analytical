f6d4b858581cc9cb7cdde63720bdaee0fbcdf495
Joshua Krall <joshuakrall@pobox.com>
2011-08-07 15:05:51 -0500

    Adjustments to class/instance/helper methods to make this Rails 2 & 3 compatible

--- lib/analytical.rb
+++ lib/analytical.rb
@@ -6,32 +6,24 @@ end
 
 module Analytical
 
-  # any method placed here will apply to ActionController::Base
   def analytical(options={})
-    send :include, InstanceMethods
-    send :include, Analytical::BotDetector
-    helper_method :analytical
-    class_inheritable_accessor :analytical_options
-
     self.analytical_options = options
 
     config_options = { :modules => [] }
-    File.open("#{Rails.root}/config/analytical.yml") do |f|
+    File.open("#{::Rails.root}/config/analytical.yml") do |f|
       file_options = YAML::load(ERB.new(f.read).result).symbolize_keys
-      env = (Rails.env || :production).to_sym
+      env = (::Rails.env || :production).to_sym
       file_options = file_options[env] if file_options.has_key?(env)
       file_options.each do |k, v|
         config_options[k.to_sym] = v.symbolize_keys
         config_options[:modules] << k.to_sym unless options && options[:modules]
       end if file_options
-    end if File.exists?("#{Rails.root}/config/analytical.yml")
+    end if File.exists?("#{::Rails.root}/config/analytical.yml")
 
     self.analytical_options = self.analytical_options.reverse_merge config_options
   end
 
   module InstanceMethods
-    # any method placed here will apply to instances
-
     def analytical
       @analytical ||= begin
         options = self.class.analytical_options.merge({
@@ -51,8 +43,25 @@ module Analytical
     end
   end
 
+  module HelperMethods
+    def analytical
+      controller.analytical
+    end
+  end
+
 end
 
 if defined?(ActionController::Base)
-  ActionController::Base.extend Analytical
+  ActionController::Base.class_eval do
+    extend Analytical
+    include Analytical::InstanceMethods
+    include Analytical::BotDetector
+    helper Analytical::HelperMethods
+
+    if ::Rails::VERSION::MAJOR < 3
+      class_inheritable_accessor :analytical_options
+    else
+      class_attribute :analytical_options
+    end
+  end
 end
