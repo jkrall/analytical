c2e5ad1973c9bd33781b604865d9d1b7762bb6c5
Duarte Henriques <duarte.henriques@seedrs.com>
2012-10-08 19:23:18 +0100

    Make javascript_helpers mandatory

--- lib/analytical/api.rb
+++ lib/analytical/api.rb
@@ -79,13 +79,11 @@ module Analytical
         tracking_javascript(:head_append),
       ]
 
-      if options[:javascript_helpers]
-        if Gem::Version.new(::Rails::VERSION::STRING) >= Gem::Version.new('3.1.0')  # Rails 3.1 lets us override views in engines
-          js << options[:controller].send(:render_to_string, :partial=>'analytical_javascript') if options[:controller]
-        else # All other rails
-          _partial_path = Pathname.new(__FILE__).dirname.join('..', '..', 'app/views/application', '_analytical_javascript.html.erb').to_s
-          js << options[:controller].send(:render_to_string, :file=>_partial_path, :layout=>false) if options[:controller]
-        end
+      if Gem::Version.new(::Rails::VERSION::STRING) >= Gem::Version.new('3.1.0')  # Rails 3.1 lets us override views in engines
+        js << options[:controller].send(:render_to_string, :partial=>'analytical_javascript') if options[:controller]
+      else # All other rails
+        _partial_path = Pathname.new(__FILE__).dirname.join('..', '..', 'app/views/application', '_analytical_javascript.html.erb').to_s
+        js << options[:controller].send(:render_to_string, :file=>_partial_path, :layout=>false) if options[:controller]
       end
 
       js.delete_if{|s| s.blank?}.join("\n")
