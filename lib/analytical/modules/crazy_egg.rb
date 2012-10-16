d489ab5d38906550f8e0a34268a5de2c007a5bfc
Duarte Henriques <duarte.henriques@seedrs.com>
2012-10-16 10:51:20 +0100

    modernize crazy_egg include

--- lib/analytical/modules/crazy_egg.rb
+++ lib/analytical/modules/crazy_egg.rb
@@ -13,7 +13,12 @@ module Analytical
           code_url = "#{options[:key].to_s[0,4]}/#{options[:key].to_s[4,4]}"
           js = <<-HTML
           <!-- Analytical Init: CrazyEgg -->
-          <script type="text/javascript" src="#{protocol}://s3.amazonaws.com/new.cetrk.com/pages/scripts/#{code_url}.js"> </script>
+          <script type="text/javascript">
+          setTimeout(function(){var a=document.createElement("script");
+          var b=document.getElementsByTagName("script")[0];
+          a.src="//dnn506yrbagrg.cloudfront.net/pages/scripts/#{code_url}.js?"+Math.floor(new Date().getTime()/3600000);
+          a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+          </script>
           HTML
           js
         end
