27edf3600232e39abbef2feb723b4b5c5dc52c44
Rajiv Aaron Manglani <rajiv@practicallygreen.com>
2013-02-11 12:26:14 -0500

    update hubspot to use async javascript.

--- lib/analytical/modules/hubspot.rb
+++ lib/analytical/modules/hubspot.rb
@@ -5,6 +5,7 @@ module Analytical
 
       def initialize(options={})
         super
+        check_hub_id
         @tracking_command_location = :body_append
       end
 
@@ -12,14 +13,25 @@ module Analytical
         init_location(location) do
           js = <<-HTML
           <!-- Analytical Init: Hubspot -->
-          <script type="text/javascript" language="javascript">
-          var hs_portalid=#{options[:portal_id]};
-          var hs_salog_version = "2.00";
-          var hs_ppa = "#{options[:domain]}";
-          document.write(unescape("%3Cscript src='" + document.location.protocol + "//" + hs_ppa + "/salog.js.aspx' type='text/javascript'%3E%3C/script%3E"));
+          <script type="text/javascript">
+            (function(d,s,i,r) {
+              if (d.getElementById(i)){return;}
+              var n=d.createElement(s),e=d.getElementsByTagName(s)[0];
+              n.id=i;n.src='//js.hubspot.com/analytics/'+(Math.ceil(new Date()/r)*r)+'/#{options[:hub_id]}.js';
+              e.parentNode.insertBefore(n, e);
+            })(document,"script","hs-analytics",300000);
           </script>
           HTML
           js
+        end
+      end
+
+      private
+
+      def check_hub_id
+        if options[:hub_id].nil?
+          raise ArgumentError, "Hubspot tracking requires 'hub_id' to be set in the 'analytical.yml' config file; " +
+            "'portal_id' and 'domain' are no longer used."
         end
       end
 
