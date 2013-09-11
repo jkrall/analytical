14359c0084c8cbdeb51aa2c6524148cd930a979a
Joshua Krall <joshuakrall@pobox.com>
2013-09-11 02:50:24 -0700

    Adding google universal tracking

---
+++ lib/analytical/modules/google_universal.rb
@@ -0,0 +1,32 @@
+module Analytical
+  module Modules
+    class GoogleUniversal
+      include Analytical::Modules::Base
+
+      def initialize(options={})
+        super
+        @tracking_command_location = :head_append
+      end
+
+      def init_javascript(location)
+        init_location(location) do
+          js = <<-HTML
+          <!-- Analytical Init: Google Universal -->
+          <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', '#{options[:key]}', '#{options[:domain]}');
+            ga('send', 'pageview');
+
+          </script>          
+          HTML
+          js
+        end
+      end
+
+    end
+  end
+end
