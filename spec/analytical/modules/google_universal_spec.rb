95dd4011682522ae8d4ca7bea045107fb7782fd7
Kamil Chmielewski <kamilski81@gmail.com>
2014-02-05 15:50:59 -0600

    fix spelling mistake

--- spec/analytical/modules/google_universal_spec.rb
+++ spec/analytical/modules/google_universal_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe "Analytical::Modules::GoogleUniveresal" do
+describe "Analytical::Modules::GoogleUniversal" do
   before(:each) do
     @parent = mock('api', :options=>{:google=>{:key=>'abc'}})
   end
