352aa9f18c0f439a23402108a5fee82d44e9ffeb
Federico Rebora <federico.rebora@carwow.co.uk>
2015-01-27 15:10:20 +0000

    Updated GTM tag

--- lib/analytical/modules/google_tag_manager.rb
+++ lib/analytical/modules/google_tag_manager.rb
@@ -30,13 +30,13 @@ module Analytical
           when :body_prepend 
             js = <<-HTML
               <!-- Google Tag Manager -->
-              <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-57THDK"
+              <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PF3LZL"
               height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
               <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-57THDK');</script>
+              })(window,document,'script','dataLayer','GTM-PF3LZL');</script>
               <!-- End Google Tag Manager -->
             HTML
             js
