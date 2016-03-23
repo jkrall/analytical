e154e58527d34b34c137c6a753228d8370638fc6
Ryan Stawarz <ryan@stawarz.com>
2016-03-23 11:07:17 -0500

    Only initialize mixpanel if the segment integration is not defined

--- lib/analytical/modules/mixpanel.rb
+++ lib/analytical/modules/mixpanel.rb
@@ -32,17 +32,19 @@ module Analytical
           js = <<-HTML
           <!-- Analytical Init: Mixpanel -->
           <script type="text/javascript">
-            (function(c,a){window.mixpanel=a;var b,d,h,e;b=c.createElement("script");
-            b.type="text/javascript";b.async=!0;b.src=("https:"===c.location.protocol?"https:":"http:")+
-            '//cdn.mxpnl.com/libs/mixpanel-2.2.min.js';d=c.getElementsByTagName("script")[0];
-            d.parentNode.insertBefore(b,d);a._i=[];a.init=function(b,c,f){function d(a,b){
-            var c=b.split(".");2==c.length&&(a=a[c[0]],b=c[1]);a[b]=function(){a.push([b].concat(
-            Array.prototype.slice.call(arguments,0)))}}var g=a;"undefined"!==typeof f?g=a[f]=[]:
-            f="mixpanel";g.people=g.people||[];h=['disable','track','track_pageview','track_links',
-            'track_forms','register','register_once','unregister','identify','alias','name_tag',
-            'set_config','people.set','people.increment','people.track_charge','people.append'];
-            for(e=0;e<h.length;e++)d(g,h[e]);a._i.push([b,c,f])};a.__SV=1.2;})(document,window.mixpanel||[]);
-            mixpanel.init("#{options[:key]}", #{config});
+            if(!(typeof analytics !== 'undefined' && typeof analytics.Integrations !== 'undefined' && analytics.Integrations.hasOwnProperty("Mixpanel"))) {
+              (function(c,a){window.mixpanel=a;var b,d,h,e;b=c.createElement("script");
+              b.type="text/javascript";b.async=!0;b.src=("https:"===c.location.protocol?"https:":"http:")+
+              '//cdn.mxpnl.com/libs/mixpanel-2.2.min.js';d=c.getElementsByTagName("script")[0];
+              d.parentNode.insertBefore(b,d);a._i=[];a.init=function(b,c,f){function d(a,b){
+              var c=b.split(".");2==c.length&&(a=a[c[0]],b=c[1]);a[b]=function(){a.push([b].concat(
+              Array.prototype.slice.call(arguments,0)))}}var g=a;"undefined"!==typeof f?g=a[f]=[]:
+              f="mixpanel";g.people=g.people||[];h=['disable','track','track_pageview','track_links',
+              'track_forms','register','register_once','unregister','identify','alias','name_tag',
+              'set_config','people.set','people.increment','people.track_charge','people.append'];
+              for(e=0;e<h.length;e++)d(g,h[e]);a._i.push([b,c,f])};a.__SV=1.2;})(document,window.mixpanel||[]);
+              mixpanel.init("#{options[:key]}", #{config});
+            }
           </script>
           HTML
           js
