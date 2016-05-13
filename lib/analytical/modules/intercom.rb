d0079752bf0bf7fd1b4d1748480843449436a8d6
Mário Nzualo <mario.nzualo@seedrs.com>
2016-05-13 15:18:11 +0100

    Track events in intercom

---
+++ lib/analytical/modules/intercom.rb
@@ -0,0 +1,20 @@
+module Analytical
+  module Modules
+    class Intercom
+      include Analytical::Modules::Base
+
+      def initialize(options={})
+        super
+        @tracking_command_location = :body_append
+      end
+
+      # Intercom indentification is being done through the intercom-rails gem
+
+      def event(*args) # name, options, callback
+        <<-JS.gsub(/^ {10}/, '')
+          Intercom('trackEvent', name, options || {});
+        JS
+      end
+    end
+  end
+end
