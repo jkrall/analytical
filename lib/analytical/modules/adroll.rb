7f6ec2ef673aa24616c1d0a8b4898524f5a0e724
JSFernandes <joao.fernandes@seedrs.com>
2016-09-01 13:04:08 +0100

    Update adroll code again
    Previous update did not include changes that I thought were superficial

--- lib/analytical/modules/adroll.rb
+++ lib/analytical/modules/adroll.rb
@@ -16,16 +16,19 @@ module Analytical
           adroll_adv_id = "#{options[:adv_id]}";
           adroll_pix_id = "#{options[:pix_id]}";
           (function () {
-          var oldonload = window.onload;
-          window.onload = function(){
-             __adroll_loaded=true;
-             var scr = document.createElement("script");
-             var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
-             scr.setAttribute('async', 'true');
-             scr.type = "text/javascript";
-             scr.src = host + "/j/roundtrip.js";
-             ((document.getElementsByTagName('head') || [null])[0] || document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
-             if(oldonload){oldonload()}};
+            var _onload = function(){
+              if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}
+              if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}
+              var scr = document.createElement("script");
+              var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+              scr.setAttribute('async', 'true');
+              scr.type = "text/javascript";
+              scr.src = host + "/j/roundtrip.js";
+              ((document.getElementsByTagName('head') || [null])[0] ||
+                  document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+            };
+            if (window.addEventListener) {window.addEventListener('load', _onload, false);}
+            else {window.attachEvent('onload', _onload)}
           }());
           </script>
           HTML
